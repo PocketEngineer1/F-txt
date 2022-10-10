17b277aecca1e8f2f396dc629f0322d48fb66e94
TURBO Dev <barbouraidan044@gmail.com>
2022-10-10 17:01:23 -0500

    commit

--- export.bat
+++ export.bat
@@ -22,4 +22,39 @@ XCOPY "Modules\Outputs\__pycache__\MicrosoftWord.cpython-310.pyc" "TMP\Modules\O
 XCOPY "Modules\Outputs\__pycache__\OpenDocumentText.cpython-310.pyc" "TMP\Modules\Outputs\" /c /y
 XCOPY "Modules\Outputs\__pycache__\StrapDownJS.cpython-310.pyc" "TMP\Modules\Outputs\" /c /y
 XCOPY "Modules\Outputs\__pycache__\TKinterApp.cpython-310.pyc" "TMP\Modules\Outputs\" /c /y
+
+IF EXIST "TMP\main.pyc" DEL "TMP\main.pyc"
+IF EXIST "TMP\utils.pyc" DEL "TMP\utils.pyc"
+IF EXIST "TMP\console.pyc" DEL "TMP\console.pyc"
+
+IF EXIST "TMP\Modules\Configure.pyc" DEL "TMP\Modules\Configure.pyc"
+
+IF EXIST "TMP\Modules\Interfaces\CLI.pyc" DEL "TMP\Modules\Interfaces\CLI.pyc"
+IF EXIST "TMP\Modules\Interfaces\GUI.pyc" DEL "TMP\Modules\Interfaces\GUI.pyc"
+IF EXIST "TMP\Modules\Interfaces\WebUI.pyc" DEL "TMP\Modules\Interfaces\WebUI.pyc"
+
+IF EXIST "TMP\Modules\Outputs\HTML.pyc" DEL "TMP\Modules\Outputs\HTML.pyc"
+IF EXIST "TMP\Modules\Outputs\Markdown.pyc" DEL "TMP\Modules\Outputs\Markdown.pyc"
+IF EXIST "TMP\Modules\Outputs\MicrosoftWord.pyc" DEL "TMP\Modules\Outputs\MicrosoftWord.pyc"
+IF EXIST "TMP\Modules\Outputs\OpenDocumentText.pyc" DEL "TMP\Modules\Outputs\OpenDocumentText.pyc"
+IF EXIST "TMP\Modules\Outputs\StrapDownJS.pyc" DEL "TMP\Modules\Outputs\StrapDownJS.pyc"
+IF EXIST "TMP\Modules\Outputs\TKinterApp.pyc" DEL "TMP\Modules\Outputs\TKinterApp.pyc"
+
+RENAME "TMP\main.cpython-310.pyc" "main.pyc"
+RENAME "TMP\utils.cpython-310.pyc" "utils.pyc"
+RENAME "TMP\console.cpython-310.pyc" "console.pyc"
+
+RENAME "TMP\Modules\Configure.cpython-310.pyc" "Configure.pyc"
+
+RENAME "TMP\Modules\Interfaces\CLI.cpython-310.pyc" "CLI.pyc"
+RENAME "TMP\Modules\Interfaces\GUI.cpython-310.pyc" "GUI.pyc"
+RENAME "TMP\Modules\Interfaces\WebUI.cpython-310.pyc" "WebUI.pyc"
+
+RENAME "TMP\Modules\Outputs\HTML.cpython-310.pyc" "HTML.pyc"
+RENAME "TMP\Modules\Outputs\Markdown.cpython-310.pyc" "Markdown.pyc"
+RENAME "TMP\Modules\Outputs\MicrosoftWord.cpython-310.pyc" "MicrosoftWord.pyc"
+RENAME "TMP\Modules\Outputs\OpenDocumentText.cpython-310.pyc" "OpenDocumentText.pyc"
+RENAME "TMP\Modules\Outputs\StrapDownJS.cpython-310.pyc" "StrapDownJS.pyc"
+RENAME "TMP\Modules\Outputs\TKinterApp.cpython-310.pyc" "TKinterApp.pyc"
+
 TITLE 
